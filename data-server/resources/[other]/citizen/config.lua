71111ae88df7e8f4ee9d119599efeece899c9bf4
cryptox1337 <dev.crypto@hotmail.com>
2021-11-10 20:42:07 +0000

    Added Einreise Script

---
+++ data-server/resources/[other]/citizen/config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+
+Config.einreise = {
+    vector3(-1092.5939941406, -2843.3728027344, 27.510042190552)
+}
+Config.angle_einreise = 22.209734
+
+
+Config.rein = {
+    vector3(-1073.3757324219, -2810.2333984375, 21.319700241089)
+}
+Config.angle_rein = 57.489734
+
+
+Config.raus = {
+    vector3(-1069.8175048828, -2743.72265625, 21.314428329468)
+}
+Config.angle_raus = 234.83993
