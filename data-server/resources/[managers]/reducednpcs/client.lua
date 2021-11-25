308e7dfd7780549fe93b0a3530c46532cc87819f
cryptox1337 <dev.crypto@hotmail.com>
2021-11-25 20:44:11 +0000

    fix random Aircraft spawn

--- data-server/resources/[managers]/reducednpcs/client.lua
+++ data-server/resources/[managers]/reducednpcs/client.lua
@@ -2,11 +2,19 @@
 DensityMultiplier = 0.0
 Citizen.CreateThread(function()
 	while true do
-	    Citizen.Wait(0)
-	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
-	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
-	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
-	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
-	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+		SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+		SetPedDensityMultiplierThisFrame(DensityMultiplier)
+		SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+		SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+		SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+		
+		local playerPed = GetPlayerPed(-1)
+		local pos = GetEntityCoords(playerPed)
+		RemoveVehiclesFromGeneratorsInArea(pos['x'] - 500.0, pos['y'] - 500.0, pos['z'] - 500.0, pos['x'] + 500.0, pos['y'] + 500.0, pos['z'] + 500.0);
+		
+		SetGarbageTrucks(0)
+		SetRandomBoats(0)
+		
+		Citizen.Wait(1)
 	end
 end)
