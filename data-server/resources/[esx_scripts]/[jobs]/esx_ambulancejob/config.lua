947ca1bb765d61042aa6fe757ad04f089d8e7f65
cryptox1337 <dev.crypto@hotmail.com>
2021-11-28 20:52:13 +0000

    added ambulance job

---
+++ data-server/resources/[esx_scripts]/[jobs]/esx_ambulancejob/config.lua
@@ -0,0 +1,47 @@
+Config                            = {}
+
+Config.DrawDistance               = 20.0 -- How close do you need to be in order for the markers to be drawn (in GTA units).
+
+Config.Marker                     = {type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false}
+
+Config.ReviveReward               = 700  -- Revive reward, set to 0 if you don't want it enabled
+Config.AntiCombatLog              = true -- Enable anti-combat logging? (Removes Items when a player logs back after intentionally logging out while dead.)
+Config.LoadIpl                    = true -- Disable if you're using fivem-ipl or other IPL loaders
+
+Config.Locale                     = 'de'
+
+Config.EarlyRespawnTimer          = 10000 * 1  -- time til respawn is available
+Config.BleedoutTimer              = 10000 * 10 -- time til the player bleeds out
+
+Config.EnablePlayerManagement     = true -- Enable society managing (If you are using esx_society).
+
+Config.RemoveWeaponsAfterRPDeath  = true
+Config.RemoveCashAfterRPDeath     = true
+Config.RemoveItemsAfterRPDeath    = true
+
+-- Let the player pay for respawning early, only if he can afford it.
+Config.EarlyRespawnFine           = false
+Config.EarlyRespawnFineAmount     = 5000
+
+Config.RespawnPoint = {coords = vector3(330.12, -595.21, 43.28), heading = 70.69}
+
+Config.Hospitals = {
+
+	CentralLosSantos = {
+
+		Blip = {
+			coords = vector3(295, -590.66, 43.05),
+			sprite = 61,
+			scale  = 1.2,
+			color  = 2
+		},
+
+		AmbulanceActions = {
+			vector3(300.26, -598.68, 42.40)
+		},
+
+		Pharmacies = {
+			vector3(309.58, -565.37, 42.40)
+		}
+	}
+}
