ed42f5c2bf1c95dd6166ae78a61e10170ee719c3
cryptox1337 <dev.crypto@hotmail.com>
2021-11-28 18:50:15 +0000

    upimized translation

--- data-server/resources/[esx_scripts]/esx_skin/locales/de.lua
+++ data-server/resources/[esx_scripts]/esx_skin/locales/de.lua
@@ -1,9 +1,9 @@
 Locales['de'] = {
-  ['skin_menu'] = 'Umkleide',
+  ['skin_menu'] = 'Laden',
   ['use_rotate_view'] = 'benutze ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ und ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ um die Sicht zu drehen.',
   ['skin'] = 'Skin ändern',
   ['saveskin'] = 'Skin speichern',
-  ['confirm_escape'] = 'Möchtest du die Umkleide verlassen? Alle änderungen werden nicht gespeichert.',
+  ['confirm_escape'] = 'Möchtest du den Laden verlassen? Alle änderungen werden nicht gespeichert.',
   ['no'] = 'nein',
   ['yes'] = 'ja',
 }
