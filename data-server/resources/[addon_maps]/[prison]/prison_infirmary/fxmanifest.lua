150e0bacba00c3686a25e5d705a43a7096d003e0
cryptox1337 <dev.crypto@hotmail.com>
2021-12-04 20:43:46 +0000

    added prison

---
+++ data-server/resources/[addon_maps]/[prison]/prison_infirmary/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+-- Leaking Hub | J. Snow | leakinghub.com
+
+creator 'sanhje'
+description 'Prison Infirmary Interior'
+website 'https://sanhje.net'
+project_page 'https://sanhje.net/projects/prisoninfirmary'
+version '1.0'
+
+this_is_a_map 'yes'
