5c9e905e2207ef99568cf1d1aa78a53f387b342e
cryptox1337 <dev.crypto@hotmail.com>
2021-12-07 14:45:02 +0000

    modified load vehicle data

--- data-server/resources/[addon_cars]/addon_cars/fxmanifest.lua
+++ data-server/resources/[addon_cars]/addon_cars/fxmanifest.lua
@@ -2,19 +2,19 @@ fx_version 'cerulean'
 game 'gta5'
 
 files {
-    'data/**/*.meta',
-    'data/**/*.xml',
-    'data/**/*.dat',
-    'data/**/*.ytyp'
+    '**/*.meta',
+    '**/*.xml',
+    '**/*.dat',
+    '**/*.ytyp'
 }
 
-data_file 'HANDLING_FILE'            'data/**/handling*.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts*.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
-data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
-data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
-data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
+data_file 'HANDLING_FILE'            '**/handling*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    '**/vehiclelayouts*.meta'
+data_file 'VEHICLE_METADATA_FILE'    '**/vehicles*.meta'
+data_file 'CARCOLS_FILE'            '**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    '**/carvariations*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' '**/*unlocks.meta'
+data_file 'PTFXASSETINFO_FILE' '**/ptfxassetinfo.meta'
 
 
 client_scripts {
