412300eda1b3fd16c4d61d2748cba76f265486d9
cryptox1337 <dev.crypto@hotmail.com>
2021-12-10 18:20:17 +0000

    translate menu dialog

--- data-server/resources/[esx_scripts]/[ui]/esx_menu_dialog/html/js/app.js
+++ data-server/resources/[esx_scripts]/[ui]/esx_menu_dialog/html/js/app.js
@@ -5,8 +5,8 @@
 			'<div class="head"><span>{{title}}</span></div>' +
 				'{{#isDefault}}<input type="text" name="value" id="inputText"/>{{/isDefault}}' +
 				'{{#isBig}}<textarea name="value"/>{{/isBig}}' +
-				'<button type="button" name="submit">Envoyer</button>' +
-				'<button type="button" name="cancel">Annuler</button>'
+				'<button type="button" name="submit">Bestätigen</button>' +
+				'<button type="button" name="cancel">Abbrechen</button>'
 			'</div>' +
 		'</div>'
 	;
