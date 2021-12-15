eae1196da8c2058d452e7483bb1ee9f96c8ed497
cryptox1337 <dev.crypto@hotmail.com>
2021-12-15 20:17:23 +0000

    modified weaponshop with society and categorys

--- data-server/resources/[esx_scripts]/[shops]/esx_weaponshop/client/main.lua
+++ data-server/resources/[esx_scripts]/[shops]/esx_weaponshop/client/main.lua
@@ -6,24 +6,32 @@ local CurrentActionMsg = ''
 local CurrentActionData = {}
 local ShopOpen = false
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-    ESX.TriggerServerCallback('esx_weaponshop:getShop', function(shopItems)
-        for k,v in pairs(shopItems) do
-            Config.Zones[k].Items = v
-        end
-    end)
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	ESX.TriggerServerCallback('esx_weaponshop:getShop', function(shopItems)
+		for k,v in pairs(shopItems) do
+			Config.Zones[k].Items = v
+		end
+	end)
 end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function (job)
+	ESX.PlayerData.job = job
+end)
+
 
 RegisterNetEvent('esx_weaponshop:sendShop')
 AddEventHandler('esx_weaponshop:sendShop', function(shopItems)
-    for k,v in pairs(shopItems) do
-        Config.Zones[k].Items = v
-    end
+	for k,v in pairs(shopItems) do
+		Config.Zones[k].Items = v
+	end
 end)
 
 function OpenBuyLicenseMenu(zone)
@@ -51,178 +59,199 @@ function OpenBuyLicenseMenu(zone)
 end
 
 function OpenShopMenu(zone)
+	if ESX.PlayerData.job == nil then
+		ESX.PlayerData = ESX.GetPlayerData()
+	end
+
 	local elements = {}
 	ShopOpen = true
+	local Allowed_Items = {}
+	local Allowed_Categorys = {}
 
-    SetNuiFocus(true, true)
+	SetNuiFocus(true, true)
 
-    if Config.Blur then
+	if Config.Blur then
 		SetTimecycleModifier('hud_def_blur') -- blur
 	end
 
-	SendNUIMessage({
-        display = true,
-        clear = true
-    })
-
+	local count = 0
 	for i=1, #Config.Zones[zone].Items, 1 do
-        local item = Config.Zones[zone].Items[i]
-        
-        SendNUIMessage({
-            itemLabel = item.label,
-            item = item.item,
-            price = item.price,
-            desc = item.desc,
-            imglink = item.imglink,
-            zone = zone
-        })
-    end
+		local item = Config.Zones[zone].Items[i]
+		if item.job ~= "" then
+			if item.job == ESX.PlayerData.job.name and ESX.PlayerData.job.can_managemoney then
+				table.insert(Allowed_Items, item)
+			end
+		else
+			table.insert(Allowed_Items, item)
+		end
+	end
+
+	for i=1, #Allowed_Items, 1 do
+		exist = false
+		for k=1, #Allowed_Categorys, 1 do	
+			if Allowed_Categorys[k] == Allowed_Items[i].category then
+				exist = true
+			end
+		end
+	
+		if not exist then
+			table.insert(Allowed_Categorys, Allowed_Items[i].category)
+		end
+	end
+
+	SendNUIMessage({
+		display = true,
+		clear = true,
+		categories = Allowed_Categorys,
+		items = Allowed_Items
+	})
+
 end
 
 AddEventHandler('esx_weaponshop:hasEnteredMarker', function(zone)
-    if zone == 'GunShop' or zone == 'BlackWeashop' then
-        CurrentAction     = 'shop_menu'
-        CurrentActionMsg  = _U('shop_menu_prompt')
-        CurrentActionData = { zone = zone }
-    end
+	if zone == 'GunShop' or zone == 'BlackWeashop' then
+		CurrentAction     = 'shop_menu'
+		CurrentActionMsg  = _U('shop_menu_prompt')
+		CurrentActionData = { zone = zone }
+	end
 end)
 
 AddEventHandler('esx_weaponshop:hasExitedMarker', function(zone)
-    CurrentAction = nil
-    ESX.UI.Menu.CloseAll()
+	CurrentAction = nil
+	ESX.UI.Menu.CloseAll()
 end)
 
 AddEventHandler('onResourceStop', function(resource)
-    if resource == GetCurrentResourceName() then
-        if ShopOpen then
-            ESX.UI.Menu.CloseAll()
-        end
-    end
+	if resource == GetCurrentResourceName() then
+		if ShopOpen then
+			ESX.UI.Menu.CloseAll()
+		end
+	end
 end)
 
 -- Create Blips
 Citizen.CreateThread(function()
-    for k,v in pairs(Config.Zones) do
-        if v.Legal then
-            for i = 1, #v.Locations, 1 do
-                local blip = AddBlipForCoord(v.Locations[i])
+	for k,v in pairs(Config.Zones) do
+		if v.Legal then
+			for i = 1, #v.Locations, 1 do
+				local blip = AddBlipForCoord(v.Locations[i])
 
-                SetBlipSprite (blip, 110)
-                SetBlipDisplay(blip, 4)
-                SetBlipScale  (blip, 0.6)
-                SetBlipColour (blip, 4)
-                SetBlipAsShortRange(blip, true)
+				SetBlipSprite (blip, 110)
+				SetBlipDisplay(blip, 4)
+				SetBlipScale  (blip, 0.6)
+				SetBlipColour (blip, 4)
+				SetBlipAsShortRange(blip, true)
 
-                BeginTextCommandSetBlipName("STRING")
-                AddTextComponentSubstringPlayerName(_U('map_blip'))
-                EndTextCommandSetBlipName(blip)
-            end
-        end
-    end
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentSubstringPlayerName(_U('map_blip'))
+				EndTextCommandSetBlipName(blip)
+			end
+		end
+	end
 end)
 
 -- Create DrawText
 function DrawText3Ds(x, y, z, text)
 	SetTextScale(0.25, 0.25)
-    SetTextFont(0)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.025+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
+	SetTextFont(0)
+	SetTextProportional(1)
+	SetTextColour(255, 255, 255, 215)
+	SetTextEntry("STRING")
+	SetTextCentre(true)
+	AddTextComponentString(text)
+	SetDrawOrigin(x,y,z, 0)
+	DrawText(0.0, 0.0)
+	local factor = (string.len(text)) / 370
+	DrawRect(0.0, 0.0+0.0125, 0.025+ factor, 0.03, 0, 0, 0, 75)
+	ClearDrawOrigin()
 end
 
 -- Display markers
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
+	while true do
+		Citizen.Wait(0)
 
-        local coords = GetEntityCoords(PlayerPedId())
+		local coords = GetEntityCoords(PlayerPedId())
 
-        for k,v in pairs(Config.Zones) do
-            for i = 1, #v.Locations, 1 do
-                if (Config.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Locations[i], true) < Config.DrawDistance) then
-                    DrawMarker(-1, v.Locations[i], 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
-                    DrawText3Ds(v.Locations[i].x, v.Locations[i].y, v.Locations[i].z + 1.0, '~g~E~w~ - Open Weapon Shop')
-                end
-            end
-        end
-    end
+		for k,v in pairs(Config.Zones) do
+			for i = 1, #v.Locations, 1 do
+				if (Config.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Locations[i], true) < Config.DrawDistance) then
+					DrawMarker(-1, v.Locations[i], 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
+					DrawText3Ds(v.Locations[i].x, v.Locations[i].y, v.Locations[i].z + 1.0, '~g~E~w~ - Open Weapon Shop')
+				end
+			end
+		end
+	end
 end)
 
 -- Enter / Exit marker events
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local coords = GetEntityCoords(PlayerPedId())
-        local isInMarker, currentZone = false, nil
+	while true do
+		Citizen.Wait(0)
+		local coords = GetEntityCoords(PlayerPedId())
+		local isInMarker, currentZone = false, nil
 
-        for k,v in pairs(Config.Zones) do
-            for i=1, #v.Locations, 1 do
-                if GetDistanceBetweenCoords(coords, v.Locations[i], true) < Config.Size.x then
-                    isInMarker, ShopItems, currentZone, LastZone = true, v.Items, k, k
-                end
-            end
-        end
+		for k,v in pairs(Config.Zones) do
+			for i=1, #v.Locations, 1 do
+				if GetDistanceBetweenCoords(coords, v.Locations[i], true) < Config.Size.x then
+					isInMarker, ShopItems, currentZone, LastZone = true, v.Items, k, k
+				end
+			end
+		end
 
-        if isInMarker and not HasAlreadyEnteredMarker then
-            HasAlreadyEnteredMarker = true
-            TriggerEvent('esx_weaponshop:hasEnteredMarker', currentZone)
-        end
-        
-        if not isInMarker and HasAlreadyEnteredMarker then
-            HasAlreadyEnteredMarker = false
-            TriggerEvent('esx_weaponshop:hasExitedMarker', LastZone)
-        end
-    end
+		if isInMarker and not HasAlreadyEnteredMarker then
+			HasAlreadyEnteredMarker = true
+			TriggerEvent('esx_weaponshop:hasEnteredMarker', currentZone)
+		end
+		
+		if not isInMarker and HasAlreadyEnteredMarker then
+			HasAlreadyEnteredMarker = false
+			TriggerEvent('esx_weaponshop:hasExitedMarker', LastZone)
+		end
+	end
 end)
 
 -- Key Controls
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if CurrentAction ~= nil then
+	while true do
+		Citizen.Wait(0)
+		if CurrentAction ~= nil then
 
-            if IsControlJustReleased(0, 38) then
-                if CurrentAction == 'shop_menu' then
-                    if Config.LicenseEnable and Config.Zones[CurrentActionData.zone].Legal then
-                        ESX.TriggerServerCallback('esx_license:checkLicense', function(hasWeaponLicense)
-                            if hasWeaponLicense then
-                                OpenShopMenu(CurrentActionData.zone)
-                            else
-                                OpenBuyLicenseMenu(CurrentActionData.zone)
-                                print("You don't have a license!")
-                            end
-                        end, GetPlayerServerId(PlayerId()), 'weapon')
-                    else
-                        OpenShopMenu(CurrentActionData.zone)
-                    end
-                end
-                CurrentAction = nil
-            end
-        end
-    end
+			if IsControlJustReleased(0, 38) then
+				if CurrentAction == 'shop_menu' then
+					if Config.LicenseEnable and Config.Zones[CurrentActionData.zone].Legal then
+						ESX.TriggerServerCallback('esx_license:checkLicense', function(hasWeaponLicense)
+							if hasWeaponLicense then
+								OpenShopMenu(CurrentActionData.zone)
+							else
+								OpenBuyLicenseMenu(CurrentActionData.zone)
+								print("You don't have a license!")
+							end
+						end, GetPlayerServerId(PlayerId()), 'weapon')
+					else
+						OpenShopMenu(CurrentActionData.zone)
+					end
+				end
+				CurrentAction = nil
+			end
+		end
+	end
 end)
 
 RegisterNUICallback('buyItem', function(data, cb)
-    ESX.TriggerServerCallback('esx_weaponshop:buyWeapon', function(success)
-        if success then
-            ESX.ShowNotification('Successful purchase')
-        else
-            PlaySoundFrontend(-1, 'ERROR', 'HUD_AMMO_SHOP_SOUNDSET', false)
-       end
-    end, data.item, data.zone)
+	ESX.TriggerServerCallback('esx_weaponshop:buyWeapon', function(success)
+		if success then
+			ESX.ShowNotification('Successful purchase')
+		else
+			PlaySoundFrontend(-1, 'ERROR', 'HUD_AMMO_SHOP_SOUNDSET', false)
+	   end
+	end, data.item, data.zone)
 end)
 
 RegisterNUICallback('focusOff', function(data, cb)
-    SetNuiFocus(false, false)
+	SetNuiFocus(false, false)
    
-    if Config.Blur then 
-        SetTimecycleModifier('default') -- remove blur
-    end
+	if Config.Blur then 
+		SetTimecycleModifier('default') -- remove blur
+	end
 end)     
--- data-server/resources/[esx_scripts]/[shops]/esx_weaponshop/html/app.js
+++ data-server/resources/[esx_scripts]/[shops]/esx_weaponshop/html/app.js
@@ -1,111 +1,190 @@
 $(document).keyup(function (e) {
-  if (e.key === 'Escape') {
-    CloseShop();
-  }
+	if (e.key === 'Escape') {
+		CloseShop();
+	}
 });
+
+
 
 function CloseShop() {
-  $('body').fadeOut(100);
-  $('.items').empty();
-  $('.container').fadeOut(100);
-  $('.modal').removeClass('visible');
-  $.post('http://esx_weaponshop/focusOff');
+	$('body').fadeOut(100);
+	$('.items').empty();
+	$('.container').fadeOut(100);
+	$('.modal').removeClass('visible');
+	$.post('http://esx_weaponshop/focusOff');
 }
 
-window.addEventListener('message', function (event) {
-  var item = event.data;
-
-  if (item.clear == true) {
-    $('.items').empty();
-  }
-
-  if (item.display == true) {
-    $('body').show();
-    $('body').fadeIn(100);
-    $('.container').show();
-  }
-
-  if (item.display == false) {
-    $('body').fadeOut(100);
-    $('.container').fadeOut(100);
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
-  $('.container').hide();
+	$('.container').hide();
 });
 
 function buyItem(item, zone) {
-  
-  $.post(
-    'http://esx_weaponshop/buyItem',
-    JSON.stringify({
-      item: item,
-      zone: zone,
-    })
-  );
+	
+	$.post(
+		'http://esx_weaponshop/buyItem',
+		JSON.stringify({
+			item: item,
+			zone: zone,
+		})
+	);
 }
 
 function showModal(item, zone, itemLabel, desc, price) {
-  $('.items').click(function () {
-    $('.modal').addClass('visible');
-    $('.modalimage').html(
-      `<img src="./img/` +
-        item +
-        `.png"><div class="itemName"><p class="modal-label">` +
-        itemLabel +
-        `</p><span class="modal-price">$` +
-        price +
-        `</span></div><p class="modal-desc">` +
-        desc +
-        `</p>`
-    );
-    $('.btn-open').html(
-      `<button class="btn-1" onclick="buyItem('` +
-        item +
-        `', '` +
-        zone +
-        `')"></button>`
-    );
-  });
+	$('.items').click(function () {
+		$('.modal').addClass('visible');
+		$('.modalimage').html(
+			`<img src="./img/` +
+				item +
+				`.png"><div class="itemName"><p class="modal-label">` +
+				itemLabel +
+				`</p><span class="modal-price">$` +
+				price +
+				`</span></div><p class="modal-desc">` +
+				desc +
+				`</p>`
+		);
+		$('.btn-open').html(
+			`<button class="btn-1" onclick="buyItem('` +
+				item +
+				`', '` +
+				zone +
+				`')"></button>`
+		);
+	});
 
-  $(document).click((event) => {
-    if ($(event.target).closest('.btn-1').length) {
-      $('.modal').removeClass('visible');
-    }
-  });
+	$(document).click((event) => {
+		if ($(event.target).closest('.btn-1').length) {
+			$('.modal').removeClass('visible');
+		}
+	});
 }
 
-window.addEventListener('message', function (event) {
-  var data = event.data;
 
-  if (data.price !== undefined) {
-    $('.items').append(
-      `
-      <div class="item" onclick="showModal('` +
-        data.item +
-        `', '` +
-        data.zone +
-        `', '` +
-        data.itemLabel +
-        `', '` +
-        data.desc +
-        `', '` +
-        data.price +
-        `')">
-        <img class="img-item" src="./img/` +
-        data.item +
-        `.png">
-          <div class="label">
-            <p class="itemString">` +
-        data.itemLabel +
-        `</p>
-            <p class="itemPrice"><span class="bg-price">$` +
-        data.price +
-        `</span></p>
-          </div>
-      </div>
-    `
-    );
-  }
+$(document).ready(function(){
+	var result
+	var category = null
+
+	function LoadItems()
+	{
+
+		$('.items').empty();
+		let index = 0	
+
+		
+		while (index < result.length) {
+			var element = result[index];
+
+			if (category == null)
+				AppendItem(element)
+			else if (category == -1)
+				AppendItem(element)
+
+			else if (element.category == category)
+				AppendItem(element)
+
+			index++;
+		}
+
+		function AppendItem(element)
+		{
+			if (element.job)
+				$('.items').append(
+					`
+					<div class="item2" onclick="showModal('` +
+						element.item +
+						`', '` +
+						element.zone +
+						`', '` +
+						element.label +
+						`', '` +
+						element.desc +" " + element.job +
+						`', '` +
+						element.price +
+						`')">
+						<img class="img-item" src="./img/` +
+						element.item +
+						`.png">
+							<div class="label">
+								<p class="itemString2">` +
+						element.label +
+						`</p>
+								<p class="itemPrice"><span class="bg-price">$` +
+						element.price +
+						`</span></p>
+							</div>
+					</div>
+				`
+				);
+			else
+				$('.items').append(
+					`
+					<div class="item" onclick="showModal('` +
+						element.item +
+						`', '` +
+						element.zone +
+						`', '` +
+						element.label +
+						`', '` +
+						element.desc +
+						`', '` +
+						element.price +
+						`')">
+						<img class="img-item" src="./img/` +
+						element.item +
+						`.png">
+							<div class="label">
+								<p class="itemString">` +
+						element.label +
+						`</p>
+								<p class="itemPrice"><span class="bg-price">$` +
+						element.price +
+						`</span></p>
+							</div>
+					</div>
+				`
+				);
+
+		}
+
+	}
+
+	$('#brand').on('change', function (){
+		category = this.value
+		LoadItems()
+	})
+
+
+	window.addEventListener('message', function (event) {
+		var data = event.data;
+
+		var categories = data.categories
+		items = data.items
+		result = items
+
+
+		if (data.clear == true) {
+			$('.items').empty();
+		}
+
+		if (data.display == true) {
+			$('body').show();
+			$('body').fadeIn(100);
+			$('.container').show();
+		}
+
+		if (data.display == false) {
+			$('body').fadeOut(100);
+			$('.container').fadeOut(100);
+		}
+
+		$('#brand').html(`<option selected value="-1">All the brands</option>`)
+		categories.forEach(element => {
+				$('#brand').append(`<option value="${element}">${element}</option>`)
+		});
+
+
+		LoadItems()
+
+	});
+
 });
