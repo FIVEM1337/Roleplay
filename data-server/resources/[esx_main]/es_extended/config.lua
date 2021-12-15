6ca7fb969253bc4aae9e45bd4ddd96f91f0bb862
cryptox1337 <dev.crypto@hotmail.com>
2021-12-15 11:41:32 +0000

    remove default cash money hud

--- data-server/resources/[esx_main]/es_extended/config.lua
+++ data-server/resources/[esx_main]/es_extended/config.lua
@@ -9,7 +9,7 @@ Config.Accounts = {
 
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.DisableWantedLevel   = true
-Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
+Config.EnableHud            = false -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.EnablePvP            = true -- enable pvp?
 Config.MaxWeight            = 24   -- the max inventory weight without backpack
 
