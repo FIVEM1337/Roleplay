c9945180eaa1b06a56a7eb73effc0ce46542386d
cryptox1337 <dev.crypto@hotmail.com>
2021-12-21 19:18:14 +0000

    added esx_tablet cad

---
+++ data-server/resources/[esx_scripts]/esx_tablet/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Emergency OS'
+description 'Tablet'
+
+client_script 'client/client.lua'
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/index.js',
+	'html/style.css',
+	'html/static/img/bildschirm.png'
+}
