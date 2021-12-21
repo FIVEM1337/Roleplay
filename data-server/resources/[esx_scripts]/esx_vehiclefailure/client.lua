d3338a3f69503f2494a756edad37bebf22d78906
cryptox1337 <dev.crypto@hotmail.com>
2021-12-21 19:34:04 +0000

    added leave engine running

--- data-server/resources/[esx_scripts]/esx_vehiclefailure/client.lua
+++ data-server/resources/[esx_scripts]/esx_vehiclefailure/client.lua
@@ -2,6 +2,25 @@ ESX               = nil
 
 
 Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		local ped = GetPlayerPed(-1)
+		
+		if DoesEntityExist(ped) and IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) and not IsPauseMenuActive() then
+			local engineWasRunning = GetIsVehicleEngineRunning(GetVehiclePedIsIn(ped, true))
+			Citizen.Wait(1000)
+			if DoesEntityExist(ped) and not IsPedInAnyVehicle(ped, false) and not IsEntityDead(ped) and not IsPauseMenuActive() then
+				local veh = GetVehiclePedIsIn(ped, true)
+				if (engineWasRunning) then
+					SetVehicleEngineOn(veh, true, true, true)
+				end
+			end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
   while ESX == nil do
 	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 	Citizen.Wait(0)
