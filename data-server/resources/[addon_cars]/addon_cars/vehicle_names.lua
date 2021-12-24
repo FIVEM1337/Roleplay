fc0d8620fe65a23b47296da9b03a89c892170ae1
cryptox1337 <dev.crypto@hotmail.com>
2021-12-24 15:59:26 +0000

    fix syntax in vehicle names

--- data-server/resources/[addon_cars]/addon_cars/vehicle_names.lua
+++ data-server/resources/[addon_cars]/addon_cars/vehicle_names.lua
@@ -46,7 +46,7 @@ Citizen.CreateThread(function()
 	AddTextEntry('mercedesgls', 'Mercedes Benz GLS Maybach')
 
 	--AUDI
-	AddTextEntry('a9', 'Audi A9'),
+	AddTextEntry('a9', 'Audi A9')
 
 	--KTM
 	AddTextEntry('duke', 'KTM Duke690')
