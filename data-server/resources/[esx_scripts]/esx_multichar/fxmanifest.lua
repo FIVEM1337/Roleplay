c7e60c5dc21ce204f4214ad0a3514c416a75cd39
cryptox1337 <dev.crypto@hotmail.com>
2022-01-03 14:06:39 +0000

    added multichar and netiveui

---
+++ data-server/resources/[esx_scripts]/esx_multichar/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server.lua',
+  }
+  
+  client_scripts {
+    "@NativeUILua_Reloaded/src/NativeUIReloaded.lua", 
+    'config.lua',
+    'client.lua',
+  }
--- data-server/resources/[esx_scripts]/esx_skin/client/main.lua
+++ data-server/resources/[esx_scripts]/esx_skin/client/main.lua
@@ -296,9 +296,7 @@ AddEventHandler('playerSpawned', function()
 
       ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
 
-        if skin == nil then
-          TriggerEvent('skinchanger:loadSkin', {sex = 0}, OpenSaveableMenu)
-        else
+        if skin ~= nil then
           TriggerEvent('skinchanger:loadSkin', skin)
         end
 
