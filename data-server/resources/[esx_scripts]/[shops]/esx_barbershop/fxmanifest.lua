73f545ccc4837c0556d89382f4fe0a1bad6adf82
cryptox1337 <dev.crypto@hotmail.com>
2022-01-04 15:11:25 +0000

    added new barbershop

---
+++ data-server/resources/[esx_scripts]/[shops]/esx_barbershop/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts {
+	"@NativeUILua_Reloaded/src/NativeUIReloaded.lua",
+	'config.lua',
+	'client.lua',
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
+	'server.lua',
+}
