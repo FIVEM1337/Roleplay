694521440e12e31d5db3ba8a9a95a3b158ff8c2f
Jokah1907 <hakan-kirtay@hotmail.com>
2022-01-04 21:07:56 +0000

    added new gang jobs

--- data-server/resources/[esx_scripts]/[jobs]/esx_gangjobs/config.lua
+++ data-server/resources/[esx_scripts]/[jobs]/esx_gangjobs/config.lua
@@ -9,16 +9,58 @@ Config.MarkerColor                = { r = 50, g = 50, b = 204 }
 gangs = {
 	{
 		job = "grove",
-		Blip = {color = 38, sprite = 423, scale = 0.8},
-		pos = vector3(84.46, -1972.56, 20.85),
+		Blip = {color = 2, sprite = 543, scale = 0.8},
+		pos = vector3(106.15, -1941.8, 20.80),
 		armory = vector3(92.25, -1962.90, 20.94),
 		boss = vector3(90.25, -1960.90, 20.94)
 	},
 	{
 		job = "bloods",
-		Blip = {color = 38, sprite = 423, scale = 0.8},
-		pos = vector3(-1025.97, -3018.49, 13.94),
-		armory = vector3(-1025.97, -3018.49, 13.94),
-		boss = vector3(-1025.97, -3018.49, 13.94)
+		Blip = {color = 1, sprite = 543, scale = 0.8},
+		pos = vector3(-1573.30, -281.20, 48.28),
+		armory = vector3(-1560.36, -274.18, 48.28),
+		boss = vector3(-1562.31, -272.42, 48.28)
+	},
+	{
+		job = "vagos",
+		Blip = {color = 5, sprite = 543, scale = 0.8},
+		pos = vector3(-1136.13, -1578.90, 4.43),
+		armory = vector3(-1128.16, -1614.16, 4.40),
+		boss = vector3(-1130.35, -1611.62, 4.40)
+	},
+	{
+		job = "lost",
+		Blip = {color = 40, sprite = 543, scale = 0.8},
+		pos = vector3(2001.25, 3065.75, 47.05),
+		armory = vector3(1999.20, 3030.68, 47.03),
+		boss = vector3(1995.10, 3024.32, 47.06)
+	},
+	{
+		job = "yakuza",
+		Blip = {color = 1, sprite = 543, scale = 0.8},
+		pos = vector3(-1473.55, 884.63, 182.95),
+		armory = vector3(-1530.50, 832.00, 181.60),
+		boss = vector3(-1529.00, 828.75, 181.60)
+	},
+	{
+		job = "ballas",
+		Blip = {color = 83, sprite = 543, scale = 0.8},
+		pos = vector3(1365.05, -578.95, 74.35),
+		armory = vector3(1342.75, -610.35, 74.40),
+		boss = vector3(1344.90, -611.75, 74.40)
+	},
+	{
+		job = "marabunta",
+		Blip = {color = 84, sprite = 543, scale = 0.8},
+		pos = vector3(1201.25, -1758.57, 39.20),
+		armory = vector3(1196.17, -1769.35, 39.50),
+		boss = vector3(1193.26, -1766.98, 39.46)
+	},
+	{
+		job = "midnight",
+		Blip = {color = 45, sprite = 543, scale = 0.8},
+		pos = vector3(-183.43, -1300.60, 30.60),
+		armory = vector3(-152.77, -1302.90, 31.30),
+		boss = vector3(-152.72, -1308.38, 31.30)
 	}
 }
