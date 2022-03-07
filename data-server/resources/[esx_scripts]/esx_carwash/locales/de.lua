52f088101610b0fe3cc85c9703474b605c097c3a
cryptox1337 <dev.crypto@hotmail.com>
2022-03-07 19:05:55 +0000

    added carwash

---
+++ data-server/resources/[esx_scripts]/esx_carwash/locales/de.lua
@@ -0,0 +1,9 @@
+Locales['de'] = {
+  ['prompt_wash'] = 'Drücke ~INPUT_CONTEXT~, um Fahrzeug zu waschen',
+  ['prompt_wash_paid'] = 'Drücke ~INPUT_CONTEXT~, um Fahrzeug für ~g~$%s~s~ zu waschen',
+  ['wash_failed'] = 'Du kannst dir keine Autowäsche leisten',
+  ['wash_failed_clean'] = 'Dein Fahrzeug muss nicht gewaschen werden.',
+  ['wash_successful'] = 'Dein Fahrzeug wurde gewaschen',
+  ['wash_successful_paid'] = 'Dein Fahrzeug wurde ~g~$%s~s~ gewaschen',
+  ['blip_carwash'] = 'Autowaschanlage',
+}
