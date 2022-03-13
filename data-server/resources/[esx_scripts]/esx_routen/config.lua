71c056ec9a808465f27e63538d50999f83dee814
xDevil933 <bruns.thomas955@gmail.com>
2022-03-13 18:05:05 +0000

    Added Teleporter Cocaine and delete Print

--- data-server/resources/[esx_scripts]/esx_routen/config.lua
+++ data-server/resources/[esx_scripts]/esx_routen/config.lua
@@ -148,165 +148,7 @@ routen = {
 				time = 3,
 				dosomething = {
 					{
-						label = "Zigaretten verkaufen",		--Anzeige Name 
-						chance = 100,						--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,				--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "money", count = 1500},
-						},
-						neededitems = {
-										{item = "wine", count = 15, remove = true},
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_SMOKING",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 1, range = 5.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-2080.40, 2611.50, 2.10),
-				npc = {model = "a_f_o_soucent_01", heading = 115.65},
-			},
-		},
------------------------------------------------------------------------------------------------------------------------------------------------------
---[[
-	Weinroute
-]]--
-		{
-			illegal = false,
-			{
-				name = "Weinnroute | Weintrauben ernten",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Weintrauben ernten",		--Anzeige Name 
-						chance = 100,				--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,		--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "grape", count = 1},
-						},
-						neededitems = {
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_GARDENER_PLANT",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 27, range = 80.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-1751.50, 1917.80, 143.00),
-				npc = false
-			},
-
-
-			{
-				name = "Weinnroute | Weintrauben eintauschen",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Weintrauben eintauschen",	--Anzeige Name 
-						chance = 100,					--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,			--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "wine", count = 1},
-						},
-						neededitems = {
-										{item = "grape", count = 15, remove = true},
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_SMOKING",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 1, range = 5.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-1905.90, 2281.30, 63.6),
-				npc = {model = "a_f_o_soucent_01", heading = 227.50},
-			},
-
-
-			{
-				name = "Weinnroute | Wein verkaufen",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Zigaretten verkaufen",		--Anzeige Name 
-						chance = 100,						--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,				--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "money", count = 1500},
-						},
-						neededitems = {
-										{item = "wine", count = 15, remove = true},
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_SMOKING",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 1, range = 5.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-2080.40, 2611.50, 2.10),
-				npc = {model = "a_f_o_soucent_01", heading = 115.65},
-			},
-		},
------------------------------------------------------------------------------------------------------------------------------------------------------
---[[
-	Weinroute
-]]--
-		{
-			illegal = false,
-			{
-				name = "Weinnroute | Weintrauben ernten",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Weintrauben ernten",		--Anzeige Name 
-						chance = 100,				--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,		--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "grape", count = 1},
-						},
-						neededitems = {
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_SMOKING",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 27, range = 80.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-1751.50, 1917.80, 143.00),
-				npc = false
-			},
-
-
-			{
-				name = "Weinnroute | Weintrauben eintauschen",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Weintrauben eintauschen",	--Anzeige Name 
-						chance = 100,					--Chance das man Items bekommt in "reciveitems"
-						removeonfail = false,			--Entferne Needed Items wenn fehlschlägt"
-						reciveitems = {
-										{item = "wine", count = 1},
-						},
-						neededitems = {
-										{item = "grape", count = 15, remove = true},
-						},
-					},
-				},
-				animation = "WORLD_HUMAN_SMOKING",
-				blip = {sprite = 93, color = 1, scale = 0.8},
-				marker = {type = 1, range = 5.0, red = 50, green = 50, blue = 204, show = false},
-				coord = vector3(-1905.90, 2281.30, 63.6),
-				npc = {model = "a_f_o_soucent_01", heading = 227.50},
-			},
-
-
-			{
-				name = "Weinnroute | Wein verkaufen",
-				title = "Wein Marken",
-				time = 3,
-				dosomething = {
-					{
-						label = "Zigaretten verkaufen",		--Anzeige Name 
+						label = "Wein verkaufen",			--Anzeige Name 
 						chance = 100,						--Chance das man Items bekommt in "reciveitems"
 						removeonfail = false,				--Entferne Needed Items wenn fehlschlägt"
 						reciveitems = {
@@ -533,7 +375,11 @@ routen = {
 				blip = {sprite = 514, color = 0, scale = 0.8},
 				marker = {type = 1, range = 5.0, red = 50, green = 50, blue = 204, show = false},
 				coord = vector3(-8.05, -759.85, 21.55),
-				npc = {model = "a_m_y_soucent_02", heading = 152.85}
+				npc = {model = "a_m_y_soucent_02", heading = 152.85},
+				teleporters = {
+                    {enter = vector3(-144.64, -835.37, 30.04), exit = vector3(-11.00,-763.14, -103.68), showmarker = true, distance = 5.00, shownotification = true},
+                    {enter = vector3(-11.00,-763.14, -103.68), exit = vector3(-144.64, -835.37, 30.04), showmarker = true, distance = 5.00, shownotification = true},
+                },
 			},
 		
 		
