7d39e06634acc4424a29fbf644e960f5f9495bf8
cryptox1337 <dev.crypto@hotmail.com>
2022-03-21 21:23:37 +0000

    updated esx_license to new esx func

--- data-server/resources/[esx_scripts]/esx_license/esx_license.sql
+++ data-server/resources/[esx_scripts]/esx_license/esx_license.sql
@@ -1,16 +1,18 @@
-USE `essentialmode`;
+USE `es_extended`;
 
 CREATE TABLE `licenses` (
 	`type` varchar(60) NOT NULL,
 	`label` varchar(60) NOT NULL,
 
 	PRIMARY KEY (`type`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `user_licenses` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT,
 	`type` varchar(60) NOT NULL,
-	`owner` varchar(60) NOT NULL,
+	`owner` VARCHAR(60) NOT NULL,
 
 	PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
--- data-server/resources/[esx_scripts]/esx_license/server/main.lua
+++ data-server/resources/[esx_scripts]/esx_license/server/main.lua
@@ -1,113 +1,74 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 function AddLicense(target, type, cb)
-	local identifier = GetPlayerIdentifier(target, 0)
+	local xPlayer = ESX.GetPlayerFromId(target)
 
-	MySQL.Async.execute('INSERT INTO user_licenses (type, owner) VALUES (@type, @owner)', {
-		['@type']  = type,
-		['@owner'] = identifier
-	}, function(rowsChanged)
-		if cb ~= nil then
+	if xPlayer then
+		MySQL.insert('INSERT INTO user_licenses (type, owner) VALUES (?, ?)', {type, xPlayer.identifier},
+		function(rowsChanged)
+			if cb then
+				cb()
+			end
+		end)
+	else
+		if cb then
 			cb()
 		end
-	end)
+	end
 end
 
 function RemoveLicense(target, type, cb)
-	local identifier = GetPlayerIdentifier(target, 0)
+	local xPlayer = ESX.GetPlayerFromId(target)
 
-	MySQL.Async.execute('DELETE FROM user_licenses WHERE type = @type AND owner = @owner', {
-		['@type']  = type,
-		['@owner'] = identifier
-	}, function(rowsChanged)
-		if cb ~= nil then
+	if xPlayer then
+		MySQL.update('DELETE FROM user_licenses WHERE type = ? AND owner = ?', {type, xPlayer.identifier},
+		function(rowsChanged)
+			if cb then
+				cb()
+			end
+		end)
+	else
+		if cb then
 			cb()
 		end
-	end)
+	end
 end
 
 function GetLicense(type, cb)
-	MySQL.Async.fetchAll('SELECT * FROM licenses WHERE type = @type', {
-		['@type'] = type
-	}, function(result)
-		local data = {
-			type  = type,
-			label = result[1].label
-		}
-
-		cb(data)
+	MySQL.scalar('SELECT label FROM licenses WHERE type = ?', {type},
+	function(result)
+		cb({type = type, label = result})
 	end)
 end
 
 function GetLicenses(target, cb)
-	local identifier = GetPlayerIdentifier(target, 0)
+	local xPlayer = ESX.GetPlayerFromId(target)
 
-	MySQL.Async.fetchAll('SELECT * FROM user_licenses WHERE owner = @owner', {
-		['@owner'] = identifier
-	}, function(result)
-		local licenses   = {}
-		local asyncTasks = {}
-
-		for i=1, #result, 1 do
-
-			local scope = function(type)
-				table.insert(asyncTasks, function(cb)
-					MySQL.Async.fetchAll('SELECT * FROM licenses WHERE type = @type', {
-						['@type'] = type
-					}, function(result2)
-						table.insert(licenses, {
-							type  = type,
-							label = result2[1].label
-						})
-
-						cb()
-					end)
-				end)
-			end
-
-			scope(result[i].type)
-
-		end
-
-		Async.parallel(asyncTasks, function(results)
-			cb(licenses)
-		end)
-
+	MySQL.query('SELECT user_licenses.type, licenses.label FROM user_licenses LEFT JOIN licenses ON user_licenses.type = licenses.type WHERE owner = ?', {xPlayer.identifier},
+	function(result)
+		cb(result)
 	end)
 end
 
 function CheckLicense(target, type, cb)
-	local identifier = GetPlayerIdentifier(target, 0)
+	local xPlayer = ESX.GetPlayerFromId(target)
 
-	MySQL.Async.fetchAll('SELECT COUNT(*) as count FROM user_licenses WHERE type = @type AND owner = @owner', {
-		['@type']  = type,
-		['@owner'] = identifier
-	}, function(result)
-		if tonumber(result[1].count) > 0 then
-			cb(true)
-		else
-			cb(false)
-		end
-
-	end)
+	if xPlayer then
+		MySQL.scalar('SELECT type FROM user_licenses WHERE type = ? AND owner = ?', {type, xPlayer.identifier},
+		function(result)
+			if result then
+				cb(true)
+			else
+				cb(false)
+			end
+		end)
+	else
+		cb(false)
+	end
 end
 
 function GetLicensesList(cb)
-	MySQL.Async.fetchAll('SELECT * FROM licenses', {
-		['@type'] = type
-	}, function(result)
-		local licenses = {}
-
-		for i=1, #result, 1 do
-			table.insert(licenses, {
-				type  = result[i].type,
-				label = result[i].label
-			})
-		end
-
-		cb(licenses)
+	MySQL.query('SELECT type, label FROM licenses',
+	function(result)
+		cb(result)
 	end)
 end
 
