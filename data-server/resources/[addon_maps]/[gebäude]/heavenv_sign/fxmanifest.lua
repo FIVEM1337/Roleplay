d7ccdd0d05916a7e26d318d0901f5ecf04d3071b
xDevil933 <bruns.thomas955@gmail.com>
2022-03-31 16:11:23 +0000

    Added Heli lift

---
+++ data-server/resources/[addon_maps]/[gebäude]/heavenv_sign/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/heavenv_sign1.ytyp'
+
+
+version '1.0.0'
+author 'Patoche#4702'
+contact 'https://discord.gg/NvrTRdh'
+
+
