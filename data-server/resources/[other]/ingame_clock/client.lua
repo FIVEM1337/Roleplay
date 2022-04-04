b42147bc1c949f2826bdc958cee6f327364cb3d7
cryptox1337 <dev.crypto@hotmail.com>
2022-04-04 14:45:33 +0000

    show clock only if minimap is rendering

--- data-server/resources/[other]/ingame_clock/client.lua
+++ data-server/resources/[other]/ingame_clock/client.lua
@@ -1,6 +1,7 @@
 local displayTime = true
 local displayDate = true
 local offset = 2
+local show = false
 
 local timeAndDateString = nil
 
@@ -23,32 +24,46 @@ function CalculateTimeToDisplay()
 	end
 end
 
+
 Citizen.CreateThread(function()
 	while true do
 		Wait(1)
-		year, month, dayOfWeek, hour, minute, secound = GetLocalTime()
-		hour = hour + offset
-		timeAndDateString = ""
-		CalculateTimeToDisplay()
-		if displayDate == true then
-			timeAndDateString = timeAndDateString .. " " .. dayOfWeek .. "." .. month .. "." .. year .. " |"
+
+		if IsMinimapRendering() then
+			show = true
+		else
+			show = false
 		end
+	end
+end)
 
-		if displayTime == true then
-			timeAndDateString = timeAndDateString .. "~y~ " .. hour .. ":" .. minute .. ":" .. secound .. "~s~ Uhr"
+Citizen.CreateThread(function()
+	while true do
+		Wait(1)
+		if show then
+			year, month, dayOfWeek, hour, minute, secound = GetLocalTime()
+			hour = hour + offset
+			timeAndDateString = ""
+			CalculateTimeToDisplay()
+			if displayDate == true then
+				timeAndDateString = timeAndDateString .. " " .. dayOfWeek .. "." .. month .. "." .. year .. " |"
+			end
+
+			if displayTime == true then
+				timeAndDateString = timeAndDateString .. "~y~ " .. hour .. ":" .. minute .. ":" .. secound .. "~s~ Uhr"
+			end
+
+			SetTextFont(0)
+			SetTextScale(0.30, 0.30)
+			SetTextColour(255, 255, 255, 255)
+			SetTextDropshadow(0, 0, 0, 0, 255)
+			SetTextEdge(1, 0, 0, 0, 255)
+			SetTextDropShadow()
+			SetTextOutline()
+			SetTextWrap(0,0.95)
+			SetTextEntry("STRING")
+			AddTextComponentString(timeAndDateString)
+			DrawText(0.017, 0.78)
 		end
-
-		  SetTextFont(0)
-		  SetTextScale(0.30, 0.30)
-		  SetTextColour(255, 255, 255, 255)
-		  SetTextDropshadow(0, 0, 0, 0, 255)
-		  SetTextEdge(1, 0, 0, 0, 255)
-		  SetTextDropShadow()
-		  SetTextOutline()
-		  SetTextWrap(0,0.95)
-		  SetTextEntry("STRING")
-
-		  AddTextComponentString(timeAndDateString)
-		  DrawText(0.017, 0.78)
-	  end
+	end
 end)
