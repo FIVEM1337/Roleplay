c7e8edfd770378257f7b51389807ab44adb4e9f0
cryptox1337 <dev.crypto@hotmail.com>
2022-04-13 19:22:35 +0200

    modified job functions

--- data-server/resources/[esx_scripts]/[jobs]/esx_jobs/client/main.lua
+++ data-server/resources/[esx_scripts]/[jobs]/esx_jobs/client/main.lua
@@ -214,14 +214,12 @@ AddEventHandler('esx_jobs:handcuff', function()
 			SetCurrentPedWeapon(playerPed, GetHashKey('WEAPON_UNARMED'), true) -- unarm player
 			SetPedCanPlayGestureAnims(playerPed, false)
 			FreezeEntityPosition(playerPed, true)
-			DisplayRadar(false)
 		else
 			ClearPedSecondaryTask(playerPed)
 			SetEnableHandcuffs(playerPed, false)
 			DisablePlayerFiring(playerPed, false)
 			SetPedCanPlayGestureAnims(playerPed, true)
 			FreezeEntityPosition(playerPed, false)
-			DisplayRadar(true)
 		end
 	end)
 end)
@@ -288,56 +286,65 @@ end)
 CreateThread(function()
 	while true do
 		Wait(0)
-
+		local playerPed = PlayerPedId()
 		if isHandcuffed then
-			local playerPed = PlayerPedId()
-			DisableControlAction(0, 1, true) -- Disable pan
-			DisableControlAction(0, 2, true) -- Disable tilt
-			DisableControlAction(0, 24, true) -- Attack
-			DisableControlAction(0, 257, true) -- Attack 2
-			DisableControlAction(0, 25, true) -- Aim
-			DisableControlAction(0, 263, true) -- Melee Attack 1
-			DisableControlAction(0, 32, true) -- W
-			DisableControlAction(0, 34, true) -- A
-			DisableControlAction(0, 31, true) -- S
-			DisableControlAction(0, 30, true) -- D
 
-			DisableControlAction(0, 45, true) -- Reload
-			DisableControlAction(0, 22, true) -- Jump
-			DisableControlAction(0, 44, true) -- Cover
-			DisableControlAction(0, 37, true) -- Select Weapon
-			DisableControlAction(0, 23, true) -- Also 'enter'?
-
-			DisableControlAction(0, 288,  true) -- Disable phone
-			DisableControlAction(0, 289, true) -- Inventory
-			DisableControlAction(0, 170, true) -- Animations
-			DisableControlAction(0, 167, true) -- Job
-
-			DisableControlAction(0, 0, true) -- Disable changing view
-			DisableControlAction(0, 26, true) -- Disable looking behind
-			DisableControlAction(0, 73, true) -- Disable clearing animation
-			DisableControlAction(2, 199, true) -- Disable pause screen
-
-			DisableControlAction(0, 59, true) -- Disable steering in vehicle
-			DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
-			DisableControlAction(0, 72, true) -- Disable reversing in vehicle
-
-			DisableControlAction(2, 36, true) -- Disable going stealth
-
-			DisableControlAction(0, 47, true)  -- Disable weapon
-			DisableControlAction(0, 264, true) -- Disable melee
-			DisableControlAction(0, 257, true) -- Disable melee
-			DisableControlAction(0, 140, true) -- Disable melee
-			DisableControlAction(0, 141, true) -- Disable melee
-			DisableControlAction(0, 142, true) -- Disable melee
-			DisableControlAction(0, 143, true) -- Disable melee
-			DisableControlAction(0, 75, true)  -- Disable exit vehicle
-			DisableControlAction(27, 75, true) -- Disable exit vehicle
-
-			if IsEntityPlayingAnim(playerPed, 'mp_arresting', 'idle', 3) ~= 1 then
-				ESX.Streaming.RequestAnimDict('mp_arresting', function()
-					TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0.0, false, false, false)
-				end)
+			if isDead then
+				isHandcuffed = false
+				ClearPedSecondaryTask(playerPed)
+				SetEnableHandcuffs(playerPed, false)
+				DisablePlayerFiring(playerPed, false)
+				SetPedCanPlayGestureAnims(playerPed, true)
+				FreezeEntityPosition(playerPed, false)
+			else
+				DisableControlAction(0, 1, true) -- Disable pan
+				DisableControlAction(0, 2, true) -- Disable tilt
+				DisableControlAction(0, 24, true) -- Attack
+				DisableControlAction(0, 257, true) -- Attack 2
+				DisableControlAction(0, 25, true) -- Aim
+				DisableControlAction(0, 263, true) -- Melee Attack 1
+				DisableControlAction(0, 32, true) -- W
+				DisableControlAction(0, 34, true) -- A
+				DisableControlAction(0, 31, true) -- S
+				DisableControlAction(0, 30, true) -- D
+	
+				DisableControlAction(0, 45, true) -- Reload
+				DisableControlAction(0, 22, true) -- Jump
+				DisableControlAction(0, 44, true) -- Cover
+				DisableControlAction(0, 37, true) -- Select Weapon
+				DisableControlAction(0, 23, true) -- Also 'enter'?
+	
+				DisableControlAction(0, 288,  true) -- Disable phone
+				DisableControlAction(0, 289, true) -- Inventory
+				DisableControlAction(0, 170, true) -- Animations
+				DisableControlAction(0, 167, true) -- Job
+	
+				DisableControlAction(0, 0, true) -- Disable changing view
+				DisableControlAction(0, 26, true) -- Disable looking behind
+				DisableControlAction(0, 73, true) -- Disable clearing animation
+				DisableControlAction(2, 199, true) -- Disable pause screen
+	
+				DisableControlAction(0, 59, true) -- Disable steering in vehicle
+				DisableControlAction(0, 71, true) -- Disable driving forward in vehicle
+				DisableControlAction(0, 72, true) -- Disable reversing in vehicle
+	
+				DisableControlAction(2, 36, true) -- Disable going stealth
+	
+				DisableControlAction(0, 47, true)  -- Disable weapon
+				DisableControlAction(0, 264, true) -- Disable melee
+				DisableControlAction(0, 257, true) -- Disable melee
+				DisableControlAction(0, 140, true) -- Disable melee
+				DisableControlAction(0, 141, true) -- Disable melee
+				DisableControlAction(0, 142, true) -- Disable melee
+				DisableControlAction(0, 143, true) -- Disable melee
+				DisableControlAction(0, 75, true)  -- Disable exit vehicle
+				DisableControlAction(27, 75, true) -- Disable exit vehicle
+	
+				if IsEntityPlayingAnim(playerPed, 'mp_arresting', 'idle', 3) ~= 1 then
+					ESX.Streaming.RequestAnimDict('mp_arresting', function()
+						TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0.0, false, false, false)
+					end)
+				end
 			end
 		else
 			Wait(500)
@@ -354,6 +361,12 @@ AddEventHandler('esx_jobs:OutVehicle', function()
 
 	local vehicle = GetVehiclePedIsIn(playerPed, false)
 	TaskLeaveVehicle(playerPed, vehicle, 16)
+
+	if isHandcuffed then
+		ClearPedSecondaryTask(playerPed)
+		Wait(200)
+		TaskPlayAnim(playerPed, 'mp_arresting', 'idle', 8.0, -8, -1, 49, 0, 0, 0, 0)
+	end
 end)
 
 function OpenJobActionsMenu(JobConfig)
@@ -376,7 +389,7 @@ function OpenJobActionsMenu(JobConfig)
 				local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
 				if closestPlayer ~= -1 and closestDistance <= 3.0 then
 					if v.label == 'identity_card' then
-						TriggerServerEvent('jsfour-idcard:open', GetPlayerServerId(PlayerId()), GetPlayerServerId(closestPlayer))
+						TriggerServerEvent('jsfour-idcard:open', GetPlayerServerId(closestPlayer), GetPlayerServerId(PlayerId()))
 					elseif v.label == 'body_search' then
 						ESX.TriggerServerCallback('esx_jobs:getOtherPlayerData', function(data)
 							if data then
