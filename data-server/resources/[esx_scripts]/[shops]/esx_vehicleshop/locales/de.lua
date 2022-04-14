0c7ddd573389b8215712f1974da66d273da98070
cryptox1337 <dev.crypto@hotmail.com>
2022-04-14 22:01:03 +0200

    removed sell vehicles

--- data-server/resources/[esx_scripts]/[shops]/esx_vehicleshop/locales/de.lua
+++ data-server/resources/[esx_scripts]/[shops]/esx_vehicleshop/locales/de.lua
@@ -3,10 +3,8 @@ Locales['de'] = {
     ['vehicle_purchased']   = 'Fahrzeug erfolgreich gekauft',
     ['not_enough_money'] = 'Du hast nicht genug Geld!',
     ['shop_menu'] = 'Drücke ~g~[E]~w~ um den Fahrzeug-Katalog zu öffnen',
-    ['sell_vehicle2'] = 'Drücke ~g~[E]~w~ to sell ~g~%s~s~ for ~g~%s~s~ $',
     ['vehicle_sold_for'] = 'Dein %s wurde verkauft für %s$',
     ['not_yours'] = 'Das Fahrzeug gehört nicht dir',
-    ['not_sellable'] = 'Das Fahrzeug ist nicht verkaufbar',
     ['watch_catalog'] = '~g~[E]~w~ Fahrzeug-Katalog',
   }
   
--- data-server/resources/[esx_scripts]/[shops]/esx_vehicleshop/locales/en.lua
+++ data-server/resources/[esx_scripts]/[shops]/esx_vehicleshop/locales/en.lua
@@ -2,7 +2,6 @@ Locales['en'] = {
     ['vehicle_purchased']   = 'Vehicle bought with success!',
     ['not_enough_money'] = 'You don\'t have enough money!',
     ['shop_menu'] = 'Press ~g~[E]~w~ to watch the catalog',
-    ['sell_vehicle'] = 'Press ~g~[E]~w~ to sell ~g~%s~s~ $',
     ['vehicle_sold_for'] = 'Your ~b~%s~s~ was ~y~sold~s~ for ~g~%s~s~€',
     ['not_yours'] = 'This vehicle is not yours',
     ['watch_catalog'] = '~g~[E]~w~ Watch catalog',
