efe53f786bf7a016c3f358d1f6f2fb8e7fdb7819
cryptox1337 <dev.crypto@hotmail.com>
2022-04-15 13:55:13 +0200

    okokContractadded vehicle sell contract

---
+++ data-server/resources/[esx_scripts]/esx_contract/client.lua
@@ -0,0 +1,105 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterNetEvent('esx_contract:GetVehicleInfo')
+AddEventHandler('esx_contract:GetVehicleInfo', function(source_playername, date, description, price, source)
+	local playerPed = PlayerPedId()
+	local coords = GetEntityCoords(playerPed)
+	local closestPlayer, playerDistance = ESX.Game.GetClosestPlayer()
+	local sellerID = source
+	target = GetPlayerServerId(closestPlayer)
+
+	if closestPlayer ~= -1 and playerDistance <= 3.0 then
+		local vehicle = ESX.Game.GetClosestVehicle(coords)
+		local vehiclecoords = GetEntityCoords(vehicle)
+		local vehDistance = GetDistanceBetweenCoords(coords, vehiclecoords, true)
+		if DoesEntityExist(vehicle) and (vehDistance <= 3) then
+			local vehProps = ESX.Game.GetVehicleProperties(vehicle)
+			ESX.TriggerServerCallback("esx_contract:GetTargetName", function(targetName)
+				SetNuiFocus(true, true)
+				SendNUIMessage({
+					action = 'openContractSeller',
+					plate = vehProps.plate,
+					model = GetLabelText(GetDisplayNameFromVehicleModel(tonumber(vehProps.model)):lower()),
+
+					source_playername = source_playername,
+					sourceID = sellerID,
+					target_playername = targetName,
+					targetID = target,
+					date = date,
+					description = description,
+					price = price
+				})
+			end, target)
+		else
+			ClearPedTasks(PlayerPedId())
+
+			TriggerEvent('dopeNotify:Alert', "", "Kein Fahrzeug in der Nähe", 5000, 'error')
+		end
+	else
+		ClearPedTasks(PlayerPedId())
+		TriggerEvent('dopeNotify:Alert', "", "Keine Person in der Nähe", 5000, 'error')
+	end
+end)
+
+RegisterNetEvent('esx_contract:OpenContractInfo')
+AddEventHandler('esx_contract:OpenContractInfo', function()
+	SetNuiFocus(true, true)
+	SendNUIMessage({
+		action = 'openContractInfo'
+	})
+end)
+
+RegisterNetEvent('esx_contract:OpenContractOnBuyer')
+AddEventHandler('esx_contract:OpenContractOnBuyer', function(data)
+	SetNuiFocus(true, true)
+	SendNUIMessage({
+		action = 'openContractOnBuyer',
+		plate = data.plateNumber,
+		model = data.vehicleModel,
+		source_playername = data.sourceName,
+		sourceID = data.sourceID,
+		target_playername = data.targetName,
+		targetID = data.targetID,
+		date = data.date,
+		description = data.description,
+		price = data.price
+	})
+end)
+
+RegisterNUICallback("action", function(data, cb)
+	if data.action == "submitContractInfo" then
+		TriggerServerEvent("esx_contract:SendVehicleInfo", data.vehicle_description, data.vehicle_price)
+		SetNuiFocus(false, false)
+	elseif data.action == "signContract1" then
+		TriggerServerEvent("esx_contract:SendContractToBuyer", data)
+		ClearPedTasks(PlayerPedId())
+		SetNuiFocus(false, false)
+	elseif data.action == "signContract2" then
+		TriggerServerEvent("esx_contract:changeVehicleOwner", data)
+		ClearPedTasks(PlayerPedId())
+		SetNuiFocus(false, false)
+	elseif data.action == "close" then
+		ClearPedTasks(PlayerPedId())
+		SetNuiFocus(false, false)
+	end
+end)
+
+RegisterNetEvent('esx_contract:startContractAnimation')
+AddEventHandler('esx_contract:startContractAnimation', function(player)
+	loadAnimDict('anim@amb@nightclub@peds@')
+	TaskStartScenarioInPlace(PlayerPedId(), 'WORLD_HUMAN_CLIPBOARD', 0, false)
+end)
+
+function loadAnimDict(dict)
+	while (not HasAnimDictLoaded(dict)) do
+		RequestAnimDict(dict)
+		Citizen.Wait(0)
+	end
+end
