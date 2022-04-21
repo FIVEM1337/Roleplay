2761a497d3d88be8c9561ef4f6433b76a2ec6786
cryptox1337 <dev.crypto@hotmail.com>
2022-04-21 19:47:32 +0200

    added new doorlock and removed old

---
+++ data-server/resources/[other]/doors_creator/locales/en.lua
@@ -0,0 +1,27 @@
+locales["en"] = {
+    ["press_to_exit"] = "Press ~INPUT_REPLAY_TOGGLE_TIMELINE~ to ~r~exit~s~",
+    ["press_to_confirm"] = "Press ~INPUT_CELLPHONE_SELECT~ to ~g~confirm~s~",
+
+    ["press_to_select"] = "Press ~INPUT_ATTACK~ to ~g~select~s~ the closest door",
+    ["hold_to_highlight"] = "Hold ~INPUT_CHARACTER_WHEEL~ to ~y~highlight~s~ all close doors",
+
+    ["icon_up_down"] = "Press ~INPUT_CELLPHONE_UP~ and ~INPUT_CELLPHONE_DOWN~ move up/down the icon",
+    ["icon_right_left"] = "Press ~INPUT_CELLPHONE_LEFT~ and ~INPUT_CELLPHONE_RIGHT~ move left/right the icon",
+
+    ["increase_distance"] = "Use ~INPUT_WEAPON_WHEEL_PREV~ to ~g~increase~s~ distance",
+    ["decrease_distance"] = "Use ~INPUT_WEAPON_WHEEL_NEXT~ to ~r~decrease~s~ distance",
+
+    ["closing_door"] = "Closing ~b~door~s~...",
+
+    ["wrong_code"] = "~r~Wrong code~s~",
+
+    ["door_already_used"] = "Door already used in doors ID ~y~%d~s~",
+
+    ["no_close_door"] = "No close door found",
+
+    ["you_need_item"] = "You need ~y~%s~s~",
+
+    ["new_model"] = "New door model: %s",
+    ["press_to_choose_new_model"] = "Press ~INPUT_VEH_HEADLIGHT~ to ~o~select~s~ a new door model",
+    ["door_already_unlocked"] = "The ~y~door~s~ is already unlocked"
+}
